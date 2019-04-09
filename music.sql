000c56287ed4d7a9fbe63b589744202f34e78e9a
Sydney <sydneyycombs@gmail.com>
2019-04-09 11:51:02 -0500

    Update music.sql

--- music.sql
+++ music.sql
@@ -6,8 +6,18 @@ DROP TABLE IF EXISTS Artist;
 CREATE TABLE IF NOT EXISTS Artist (
   stage_name varchar(50) NOT NULL,
   genre varchar(20) NOT NULL,
-  PRIMARY KEY (stage_name),
+  PRIMARY KEY (stage_name)
 );
+
+--
+-- Adding sample data to Artist table
+--
+
+INSERT INTO Artist (stage_name, genre) VALUES('Sia', 'Pop');
+INSERT INTO Artist (stage_name, genre) VALUES('Coldplay', 'Rock');
+INSERT INTO Artist (stage_name, genre) VALUES('Rihanna', 'R&B');
+INSERT INTO Artist (stage_name, genre) VALUES('Taylor Swift', 'Pop');
+INSERT INTO Artist (stage_name, genre) VALUES('Queen', 'Rock');
 
 --
 -- Member table
@@ -17,9 +27,26 @@ DROP TABLE IF EXISTS Member;
 CREATE TABLE IF NOT EXISTS Member (
   stage_name varchar(50) NOT NULL,
   member_name varchar(50) NOT NULL,
-  PRIMARY KEY (stage_name, member_name)
+  PRIMARY KEY (stage_name, member_name),
   KEY stage_name (stage_name)
 );
+
+--
+-- Adding sample data to Member table
+--
+
+INSERT INTO Member (stage_name, member_name) VALUES('Sia', 'Sia Furler');
+INSERT INTO Member (stage_name, member_name) VALUES('Coldplay', 'Guy Berryman');
+INSERT INTO Member (stage_name, member_name) VALUES('Coldplay', 'Jonny Buckland');
+INSERT INTO Member (stage_name, member_name) VALUES('Coldplay', 'Will Champion');
+INSERT INTO Member (stage_name, member_name) VALUES('Coldplay', 'Chris Martin');
+INSERT INTO Member (stage_name, member_name) VALUES('Coldplay', 'Phil Harvey');
+INSERT INTO Member (stage_name, member_name) VALUES('Rihanna', 'Robyn Rihanna Fenty');
+INSERT INTO Member (stage_name, member_name) VALUES('Taylor Swift', 'Taylor Swift');
+INSERT INTO Member (stage_name, member_name) VALUES('Queen', 'Freddie Mercury');
+INSERT INTO Member (stage_name, member_name) VALUES('Queen', 'John Deacon');
+INSERT INTO Member (stage_name, member_name) VALUES('Queen', 'Brian May');
+INSERT INTO Member (stage_name, member_name) VALUES('Queen', 'Roger Taylor');
 
 --
 -- Album table
@@ -31,8 +58,25 @@ CREATE TABLE IF NOT EXISTS Album (
   name varchar(30) NOT NULL,
   genre varchar(20) NOT NULL,
   year year NOT NULL,
-  PRIMARY KEY (album_id),
+  PRIMARY KEY (album_id)
 );
+
+--
+-- Adding sample data to Album table
+--
+
+INSERT INTO Album (name, genre, year) VALUES('This Is Acting', 'Pop', '2016');
+INSERT INTO Album (name, genre, year) VALUES('1000 Forms of Fear', 'Electropop','2004');
+INSERT INTO Album (name, genre, year) VALUES('Parachutes', 'Alternative Rock', '2000');
+INSERT INTO Album (name, genre, year) VALUES('A Head Full of Dreams', 'Pop', '2015');
+INSERT INTO Album (name, genre, year) VALUES('Good Girl Gone Bad', 'Pop', '2007');
+INSERT INTO Album (name, genre, year) VALUES('Anti', 'Pop', '2016');
+INSERT INTO Album (name, genre, year) VALUES('Fearless', 'Country Pop', '2008');
+INSERT INTO Album (name, genre, year) VALUES('1989', 'Pop', '2014');
+INSERT INTO Album (name, genre, year) VALUES('Reputation', 'Pop', '2017');
+INSERT INTO Album (name, genre, year) VALUES('Sheer Heart Attack', 'Rock', '1974');
+INSERT INTO Album (name, genre, year) VALUES('A Night at the Opera', 'Rock', '1975');
+INSERT INTO Album (name, genre, year) VALUES('Innuendo', 'Rock', '1991');
 
 --
 -- Song table
@@ -48,6 +92,24 @@ CREATE TABLE IF NOT EXISTS Song (
   KEY album_id (album_id)
 );
 
+INSERT INTO Song (name, genre, album_id) VALUES('Alive', 'Pop', 1);
+INSERT INTO Song (name, genre, album_id) VALUES('Cheap Thrills', 'Pop', 1);
+INSERT INTO Song (name, genre, album_id) VALUES('Chandelier', 'Pop', 2);
+INSERT INTO Song (name, genre, album_id) VALUES('Elastic Heart', 'Pop', 2);
+INSERT INTO Song (name, genre, album_id) VALUES('Yellow', 'Alternative Rock', 3);
+INSERT INTO Song (name, genre, album_id) VALUES('A Head Full of Dreams', 'Pop Rock', 4);
+INSERT INTO Song (name, genre, album_id) VALUES('Umbrella', 'Pop', 5);
+INSERT INTO Song (name, genre, album_id) VALUES('Don\'t Stop the Music', 'Dance', 5);
+INSERT INTO Song (name, genre, album_id) VALUES('Work', 'Dancehall', 6);
+INSERT INTO Song (name, genre, album_id) VALUES('Look What You Made Me Do', 'Pop', 9);
+INSERT INTO Song (name, genre, album_id) VALUES('Fifteen', 'Country Pop', 7);
+INSERT INTO Song (name, genre, album_id) VALUES('You Belong With Me', 'Country Pop', 7);
+INSERT INTO Song (name, genre, album_id) VALUES('Blank Space', 'Pop', 8);
+INSERT INTO Song (name, genre, album_id) VALUES('Shake It Off', 'Pop', 8);
+INSERT INTO Song (name, genre, album_id) VALUES('Innuendo', 'Progressive Rock', 12);
+INSERT INTO Song (name, genre, album_id) VALUES('Killer Queen', 'Art Pop', 10);
+INSERT INTO Song (name, genre, album_id) VALUES('Bohemian Rhapsody', 'Progressive Rock', 11);
+
 --
 -- Artist_has_album
 --
@@ -62,6 +124,24 @@ CREATE TABLE IF NOT EXISTS Artist_has_album (
 );
 
 --
+-- Adding sample data to Artist_has_album
+--
+
+INSERT INTO Artist_has_album (stage_name, album_id) VALUES('Sia', 1);
+INSERT INTO Artist_has_album (stage_name, album_id) VALUES('Sia', 2);
+INSERT INTO Artist_has_album (stage_name, album_id) VALUES('Coldplay', 3);
+INSERT INTO Artist_has_album (stage_name, album_id) VALUES('Coldplay', 4);
+INSERT INTO Artist_has_album (stage_name, album_id) VALUES('Rihanna', 5);
+INSERT INTO Artist_has_album (stage_name, album_id) VALUES('Rihanna', 6);
+INSERT INTO Artist_has_album (stage_name, album_id) VALUES('Taylor Swift', 7);
+INSERT INTO Artist_has_album (stage_name, album_id) VALUES('Taylor Swift', 8);
+INSERT INTO Artist_has_album (stage_name, album_id) VALUES('Taylor Swift', 9);
+INSERT INTO Artist_has_album (stage_name, album_id) VALUES('Queen', 10);
+INSERT INTO Artist_has_album (stage_name, album_id) VALUES('Queen', 11);
+INSERT INTO Artist_has_album (stage_name, album_id) VALUES('Queen', 12);
+
+
+--
 -- Artist_has_song
 --
 
@@ -73,6 +153,25 @@ CREATE TABLE IF NOT EXISTS Artist_has_song (
   KEY stage_name (stage_name),
   KEY song_id (song_id)
 );
+
+INSERT INTO Artist_has_song (stage_name, song_id) VALUES('Sia', 1);
+INSERT INTO Artist_has_song (stage_name, song_id) VALUES('Sia', 2);
+INSERT INTO Artist_has_song (stage_name, song_id) VALUES('Sia', 3);
+INSERT INTO Artist_has_song (stage_name, song_id) VALUES('Sia', 4);
+INSERT INTO Artist_has_song (stage_name, song_id) VALUES('Coldplay', 5);
+INSERT INTO Artist_has_song (stage_name, song_id) VALUES('Coldplay', 6);
+INSERT INTO Artist_has_song (stage_name, song_id) VALUES('Rihanna', 7);
+INSERT INTO Artist_has_song (stage_name, song_id) VALUES('Rihanna', 8);
+INSERT INTO Artist_has_song (stage_name, song_id) VALUES('Rihanna', 9);
+INSERT INTO Artist_has_song (stage_name, song_id) VALUES('Taylor Swift', 10);
+INSERT INTO Artist_has_song (stage_name, song_id) VALUES('Taylor Swift', 11);
+INSERT INTO Artist_has_song (stage_name, song_id) VALUES('Taylor Swift', 12);
+INSERT INTO Artist_has_song (stage_name, song_id) VALUES('Taylor Swift', 13);
+INSERT INTO Artist_has_song (stage_name, song_id) VALUES('Taylor Swift', 14);
+INSERT INTO Artist_has_song (stage_name, song_id) VALUES('Queen', 15);
+INSERT INTO Artist_has_song (stage_name, song_id) VALUES('Queen', 16);
+INSERT INTO Artist_has_song (stage_name, song_id) VALUES('Queen', 17);
+
 
 --
 -- User table
