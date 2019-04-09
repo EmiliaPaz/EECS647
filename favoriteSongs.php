e489e18d8159bd6d82ad88c5de3ffa784de4648e
Sydney Combs <sydneycombs@ku.edu>
2019-04-09 15:01:07 -0500

    Added favorite artists display and changed favoriteSongs and favoriteArtists titles to Song and Artists

--- favoriteSongs.php
+++ favoriteSongs.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Dashboard</title>
+    <title>Songs</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
