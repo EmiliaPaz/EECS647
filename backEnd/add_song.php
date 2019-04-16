65bfdaab84d8cea895778561bab7713bd9410ae6
Sydney Combs <sydneycombs@ku.edu>
2019-04-16 12:13:54 -0500

    Song search and favorite added

---
+++ backEnd/add_song.php
@@ -0,0 +1,18 @@
+<?php
+
+  $mysqli = new mysqli("mysql.eecs.ku.edu", "csydney", "Jaisai4e", "csydney");
+  session_start();
+  $username = $_SESSION['username'];
+  $song_id = $_GET['song_id'];
+
+  $query = "INSERT INTO Favorite_songs (song_id, username) VALUES ('$song_id', '$username')";
+  if ($result = $mysqli->query($query))
+  {
+    header('Location: ../favoriteSongs.php');
+  }
+  else
+  {
+    header('Location: ../favoriteSongs.php');
+  }
+  
+?>
--- backEnd/find_song.php
+++ backEnd/find_song.php
@@ -1,7 +1,85 @@
-<?php
-  $song_name = $_POST['song_name'];
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <title>Song Search</title>
+     <!-- Required meta tags -->
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+     <!-- Bootstrap CSS -->
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+     <!-- CSS -->
+     <link rel="stylesheet" href="css/general.css">
+ </head>
 
-  $mysqli = new mysqli("mysql.eecs.ku.edu", "csydney", "Jaisai4e", "csydney");
-  $query = "";
 
- ?>
+ <body>
+
+     <!-- Header -->
+     <?php
+         include('templates/navbar.php');
+     ?>
+
+     <!-- Credentials -->
+     <?php
+         // include('templates/sql_credentials.php');
+         $mysqli = new mysqli("mysql.eecs.ku.edu", "csydney", "Jaisai4e", "csydney");
+         session_start();
+        $username = $_SESSION['username'];
+        $song_name = $_POST['song_name'];
+     ?>
+
+      <!-- Content -->
+     <div class="content">
+         <h2> Songs Like "<?php echo $song_name; ?>"  </h2>
+
+           <!-- <form action="add_song.php" method="post"> -->
+             <table class="table thead-light table-hover" >
+                 <thead class="thead-light">
+                     <th scope="col"> ID </th>
+                     <th scope="col"> Name </th>
+                     <th scope="col"> Genre </th>
+                     <th scope="col"> </th>
+                 </thead>
+
+                 <?php
+                   $query = "SELECT * FROM Song WHERE name LIKE '%$song_name%'";
+                   if ($result = $mysqli->query($query))
+                   {
+                     while ($row = $result->fetch_assoc())
+                     {
+                       $song_id = $row['song_id'];
+                       $name = $row['name'];
+                       $genre = $row['genre'];
+                       ?>
+                       <tr>
+                           <td> <?php echo $song_id; ?> </td>
+                           <td> <?php echo $name; ?> </td>
+                           <td> <?php echo $genre; ?> </td>
+                           <td>
+                             <a href="add_song.php?song_id=<?php echo $song_id ?>"> Add favorite </a>
+                             <!-- <input type="hidden" name ="song_id"> -->
+                             <!-- <input type="submit" value="Add to favorites">  -->
+                           </td>
+                       </tr>
+                       <?php
+                     }
+                     $result->free();
+                   }
+                 ?>
+             </table>
+           <!-- </form> -->
+     </div>
+
+     <div>
+       <form action="../favoriteSongs.php" method="post">
+         <input type="submit" value="Go back">
+       </form>
+     </div>
+
+     <!-- Optional JavaScript -->
+     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+ </body>
+ </html>
